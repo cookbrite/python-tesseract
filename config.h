71405493719111fc63a1aff6c85f19620307d540
Morgan Doocy <morgan@cookbrite.com>
2016-03-20 23:49:33 -0700

    Remove files deleted by 'python setup.py clean' from revision control
    - This also modified tesseract.i.

--- config.h
+++
@@ -1,7 +0,0 @@
-#ifndef __python2__
-	#define __python2__
-#endif
-#pragma once
-#define __windows__
-
-#include "fmemopen.h"
--- main.h
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-bool isLibTiff();
-bool isLibLept();
-int*  AllWordConfidences(tesseract::TessBaseAPI* api);
-char* ProcessPagesWrapper(const char* image,tesseract::TessBaseAPI* api);
-char* ProcessPagesPix(const char* image,tesseract::TessBaseAPI* api);
-char* ProcessPagesFileStream(const char* image,tesseract::TessBaseAPI* api);
-char* ProcessPagesBuffer(char* buffer, int fileLen, tesseract::TessBaseAPI* api);
-char* ProcessPagesRaw(const char* image,tesseract::TessBaseAPI* api);
